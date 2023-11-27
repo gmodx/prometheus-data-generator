e39060e07f85f3a778549bb5c0120428338eb9fa
qinxiaowen <qinxiaowen@virtaitech.com>
2023-11-27 12:51:39 +0000

    update

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+const (
+	Name       = "Prometheus data generator"
+	binaryName = "prometheus_data_generator"
+)
+
+type Config struct {
+}
+
+func getWd() string {
+	wd, _ := os.Getwd()
+	return wd
+}
+
+func main() {
+	rootCmd := RootCmd()
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
+
+var (
+	configFile string
+)
+
+func RootCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   binaryName,
+		Short: binaryName,
+		Run: func(cmd *cobra.Command, args []string) {
+
+		},
+	}
+
+	cmd.Flags().StringVar(&configFile, "config.file", "config.yaml", "configuration file path")
+	return cmd
+}
